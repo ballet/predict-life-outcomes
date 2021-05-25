768bb7c8748ff1c08184070a8ddfbfd53be12b9f
Micah Smith <micahs@mit.edu>
2021-05-25 00:17:38 -0400

    Refactor existing features to use new subset transformer sugar

--- src/fragile_families/features/contrib/user_micahjsmith/feature_56115b1f_cdac_4289_9126_ec1bb18755d0.py
+++ src/fragile_families/features/contrib/user_micahjsmith/feature_56115b1f_cdac_4289_9126_ec1bb18755d0.py
@@ -1,53 +1,49 @@
 # include any imports used in this feature right here (within this code cell)
 from ballet import Feature
-from ballet.eng import NullFiller, ColumnSelector
+from ballet.eng import NullFiller, ValueReplacer
 import numpy as np
 
 # what are the input columns to this feature?
-# type - Union[str, List[str]]
 input = ["m1a4", "cf1cohm", "f1b2", "f2c5"]
 
-
-def clean(df):
-    df = df.copy()
-    df.replace(-9, np.nan, inplace=True)
-
-    # clean m1a4
-    df["m1a4"] = df["m1a4"].replace(2, 0).replace(3, 0).replace(-3, np.nan)
-
-    df["f1b2"].replace(2, 0, inplace=True)
-    df["f2c5"].replace(2, 0, inplace=True)
-    df["f2c5"].replace(-6, 0, inplace=True)
-
-    married_missing = df["m1a4"].isnull()
-    df["m1a4"].where(df["m1a4"].notnull(), df["f1b2"], inplace=True)
-    df["m1a4"].where(df["m1a4"].notnull(), df["f2c5"], inplace=True)
-
-    # clean cf1cohm
-    df["cf1cohm"].fillna(0, inplace=True)
-
-    # replace for nonmarried
-    df["m1a4"].where(df["m1a4"] == 1, df["cf1cohm"], inplace=True)
-
-    return df
-
-
 # what transformations do you want to apply to these specific input columns?
-# type - function/lambda, transformer-like (object with fit and transform methods), or list of functions/transformers
 transformer = [
-    clean,
+    ValueReplacer(-9, np.nan),
+    # clean m1a4
+    ("m1a4", [ValueReplacer(2, 0), ValueReplacer(3, 0), ValueReplacer(-3, np.nan),]),
+    # clean f1b2
+    ("f1b2", ValueReplacer(2, 0)),
+    # clean f2c5
+    ("f2c5", [ValueReplacer(2, 0), ValueReplacer(-6, 0),]),
+    # replace m1a4 with f1b2 if it is missing
+    (
+        ["m1a4", "f1b2"],
+        lambda df: df["m1a4"].where(df["m1a4"].notnull(), df["f1b2"]),
+        "m1a4",
+    ),
+    # replace m1a4 with f2c5 it is is still missing
+    (
+        ["m1a4", "f2c5"],
+        lambda df: df["m1a4"].where(df["m1a4"].notnull(), df["f2c5"]),
+        "m1a4",
+    ),
+    # clean cf1cohm
+    ("cf1cohm", NullFiller()),
+    # replace for nonmarried
+    (["m1a4", "cf1cohm"], lambda df: df["m1a4"].where(df["m1a4"] == 1, df["cf1cohm"])),
+    # fill any remaining missing values with 0
     NullFiller(),
-    ColumnSelector("m1a4"),
 ]
 
 # what is a brief name of this feature?
-# type - str
 name = "married or cohabiting at birth"
 
-# what is a longer human-readable description for this feature? you can include more background on your calculations or thinking
-# type - Optional[str]
+# what is a longer human-readable description for this feature? you can include
+# more background on your calculations or thinking
 description = """\
-Whether the baby's parents are either married or cohabiting at the time of the baby's birth. Try to impute married by father survey. If cannot impute, then default to 0.
+Whether the baby's parents are either married or cohabiting at the time of the
+baby's birth. Try to impute married by father survey. If cannot impute, then
+default to 0.
 """
 
 # put it all together!
