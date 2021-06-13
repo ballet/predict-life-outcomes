367ea95ad15855afc39e1f6bbd25f749da4e9c3c
leix28 <leix28@users.noreply.github.com>
2021-06-13 06:15:12 +0000

    Add new feature

---
+++ src/fragile_families/features/contrib/user_leix28/feature_d09993a7_36e6_4fd5_88eb_81abd8bbd901.py
@@ -0,0 +1,25 @@
+# include any imports used in this feature right here (within this code cell)
+from ballet import Feature
+
+# what are the input columns to this feature?
+# type- Union[str, List[str]]
+input = [x for x in list(X_df.columns) if "t5b1" in x]
+
+# what transformations do you want to apply to these specific input columns?
+# type- function/lambda, transformer-like (object with fit and transform
+#       methods), or list of functions/transformers
+def transformer(df):
+    return df.values.clip(1, 4).mean(axis=1)
+
+
+# what is a brief name of this feature?
+# type- str
+name = "t5 school work and learning"
+
+# what is a longer human-readable description for this feature? you can include
+# more background on your calculations or thinking
+# type- Optional[str]
+description = "t5 school work and learning, average score of t5b2"
+
+# put it all together!
+feature = Feature(input, transformer, name, description)
