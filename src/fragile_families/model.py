971dba735105b6d00ae9c3b975a647534afe624c
Micah Smith <micahs@mit.edu>
2021-07-05 17:32:08 -0400

    Load pipeline and specify target in init_params

--- src/fragile_families/model.py
+++ src/fragile_families/model.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from ballet.eng.base import BaseTransformer
 
 from mlblocks import MLPipeline, add_pipelines_path, add_primitives_path
 from mlblocks import load_pipeline as _load_pipeline
@@ -16,8 +17,30 @@ PIPELINES = [
 ]
 DEFAULT_PIPELINE = 'ballet_rf_regressor'
 
+TARGETS = [
+    'materialHardship',
+    'gpa',
+    'grit',
+    'eviction',
+    'layoff',
+    'jobTraining',
+]
+DEFAULT_TARGET = 'materialHardship'
 
-def load_pipeline(name: str = DEFAULT_PIPELINE) -> MLPipeline:
+ENCODER_BLOCK_ID = 'fragile_families.encode_target#1'
+
+
+def load_pipeline(
+    name: str = DEFAULT_PIPELINE, target: str = DEFAULT_TARGET
+) -> MLPipeline:
     if name not in PIPELINES:
         raise ValueError(f'Pipeline {name!r} is not supported')
-    return MLPipeline(_load_pipeline(name))
+    if target not in TARGETS:
+        raise ValueError(f'Target {target!r} is not supported')
+    init_params = {ENCODER_BLOCK_ID: {'target': target}}
+    return MLPipeline(_load_pipeline(name), init_params=init_params)
+
+
+def get_encoder_from_pipeline(pipeline: MLPipeline) -> BaseTransformer:
+    """Get the target encoder instance from within the pipeline"""
+    return pipeline.blocks[ENCODER_BLOCK_ID].instance
